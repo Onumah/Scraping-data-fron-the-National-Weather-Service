771504fe46d453086684a79a31d6fe46b43bcfe3
10831391 <109426953+Onumah@users.noreply.github.com>
2022-09-04 10:57:36 +0000

    Delete forecast.py

--- forecast.py
+++
@@ -1,6 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
-
-page = requests.get('https://forecast.weather.gov/MapClick.php?lat=40.263690000000054&lon=-89.23509999999999#.YxR1IXbP3IU')
-print(page)
